487ec6967515d9a88395fd76ba832cda5db966f2
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2023-11-13 13:27:33 +0200

    unique username strong pass is EGY phone num

--- src/BankRegister.java
+++ src/BankRegister.java
@@ -1,22 +1,44 @@
 import java.util.Scanner;
 
-public class BankRegister implements Register {
+public class BankRegister extends Register {
     public BankUser register() {
+        Database db = new Database();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter username: ");
-        String userName = sc.next();
-        System.out.println("Enter password: ");
-        String password = sc.next();
-        System.out.println("Enter bank account number: ");
-        String bankAccNum = sc.next();
-        System.out.println("Enter balance: ");
-        double balance = sc.nextDouble();
-        System.out.println("Enter phone number: ");
-        String phoneNum = sc.next();
-        BankAPI bankAPI = new BankAPI();
-        if(!bankAPI.validBankACC(bankAccNum, phoneNum)){
-            System.out.println("Invalid Bank Account");
-            return null;
+        String userName, password, bankAccNum, phoneNum;
+        double balance;
+        while (true){
+            System.out.println("Enter username: ");
+            userName = sc.next();
+            // @Abc1234
+            System.out.println("Enter password: ");
+            password = sc.next();
+            System.out.println("Enter bank account number: ");
+            bankAccNum = sc.next();
+            System.out.println("Enter balance: ");
+            balance = sc.nextDouble();
+            System.out.println("Enter phone number: ");
+            phoneNum = sc.next();
+            BankAPI bankAPI = new BankAPI();
+            
+            if (!isStrongPassword(password)) {
+                System.out.println("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+                continue;
+            }
+            if (!isEGYPhoneNum(phoneNum)) {
+                System.out.println("Phone number must be 11 digits and start with 01.");
+                continue;
+            }
+            if (!db.isUniqueUserName(userName)) {
+                System.out.println("Username already exists.");
+                continue;
+            }
+            if(!bankAPI.validBankACC(bankAccNum, phoneNum)){
+                System.out.println("Invalid Bank Account");
+                return null;
+            }
+            if (isStrongPassword(password) && isEGYPhoneNum(phoneNum) && db.isUniqueUserName(userName)) {
+                break;
+            }
         }
         BankUser user = new BankUser(userName, password, bankAccNum, balance, phoneNum);
         return user;
--- src/WalletRegister.java
+++ src/WalletRegister.java
@@ -1,20 +1,42 @@
 import java.util.Scanner;
 
-public class WalletRegister implements Register {
+public class WalletRegister extends Register {
     public WalletUser register() {
+        Database db = new Database();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter username: ");
-        String userName = sc.next();
-        System.out.println("Enter password: ");
-        String password = sc.next();
-        System.out.println("Enter wallet balance: ");
-        double balance = sc.nextDouble();
-        System.out.println("Enter phone number: ");
-        String phoneNum = sc.next();
-        WalletAPI walletAPI = new WalletAPI();
-        if(!walletAPI.validWalletACC(phoneNum)){
-            System.out.println("Invalid Wallet Account");
-            return null;
+        String userName, password, phoneNum;
+        double balance;
+        while (true){
+            System.out.println("Enter username: ");
+            userName = sc.next();
+            // @Abc1234
+            System.out.println("Enter password: ");
+            password = sc.next();
+            System.out.println("Enter wallet balance: ");
+            balance = sc.nextDouble();
+            System.out.println("Enter phone number: ");
+            phoneNum = sc.next();
+            WalletAPI walletAPI = new WalletAPI();
+            
+            if (!isStrongPassword(password)) {
+                System.out.println("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
+                continue;
+            }
+            if (!isEGYPhoneNum(phoneNum)) {
+                System.out.println("Phone number must be 11 digits and start with 01.");
+                continue;
+            }
+            if (!db.isUniqueUserName(userName)) {
+                System.out.println("Username already exists.");
+                continue;
+            }
+            if(!walletAPI.validWalletACC(phoneNum)){
+                System.out.println("Invalid Wallet Account");
+                return null;
+            }
+            if (isStrongPassword(password) && isEGYPhoneNum(phoneNum) && db.isUniqueUserName(userName)) {
+                break;
+            }
         }
         WalletUser user = new WalletUser(userName, password, balance, phoneNum);
         return user;
